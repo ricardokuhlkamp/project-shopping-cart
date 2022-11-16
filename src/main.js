ad5f7efa57881afe5e018d1b8e13c43b527abce3
ricardokuhlkamp <ric.kds@hotmail.com>
2022-11-16 17:59:57 -0300

    inserção das imagens dos produtos na tela

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
-// import { fetchProductsList } from './helpers/fetchFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductImageElement } from './helpers/shopFunctions';
+
+const products = document.querySelector('.products');
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+// console.log(await fetchProductsList('computador'));
+
+async function criaListagemDeProdutos(produto) {
+  const dataResults = await fetchProductsList(produto);
+
+  dataResults.forEach((dataResult) => {
+    const retornoImg = createProductImageElement(dataResult.thumbnail);
+    products.appendChild(retornoImg);
+  });
+
+  // createProductImageElement(dataResult.thumbnail);
+  // createCustomElement(produto, )
+}
+
+criaListagemDeProdutos('computador');
