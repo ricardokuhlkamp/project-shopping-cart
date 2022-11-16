00bc5c2ed7b1bbab3aa1885604271b6fcfe8c271
ricardokuhlkamp <ric.kds@hotmail.com>
2022-11-16 16:12:08 -0300

    implentando testes no arquivo fetchProductsList.test.js

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -4,7 +4,8 @@ import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('Execute a função fetchProductsList com o argumento "computador" e teste se fetch foi chamada', () => {
-    
+  it('Teste se fetchProductsList é uma função', () => {
+    expect(typeof fetchProduct).toBe('function');
   });
+
 });
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,27 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toBe('function');
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador');
+    expect(fetch).toBeCalled();
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetch).toHaveBeenCalledWith(endpoint);
   });
+  it('Teste se o retorno da função fetchProductsList com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo', async () => {    
+    await expect(fetchProductsList('computador')).toMatchObject(computadorSearch);
+  });
+  it("Teste se, ao chamar a função fetchProductsList sem argumento, retorna um erro com a mensagem: Termo de busca não informado", async () => {
+    await expect(fetchProductsList()).rejects.toThrow('Termo de busca não informado');
+  })
 
   // it('...', () => {
   // });
 });
+
+
