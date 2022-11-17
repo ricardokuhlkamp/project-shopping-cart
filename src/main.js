7305e4748d8a0877c1b42c5dc4139e70851f3f71
ricardokuhlkamp <ric.kds@hotmail.com>
2022-11-16 22:34:04 -0300

    requisito 04 aparentemente resolvido porém ai há a necessidade de checa-lo

--- src/main.js
+++ src/main.js
@@ -4,18 +4,32 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 
 const products = document.querySelector('.products');
+const tagH2 = document.createElement('h2');
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 // console.log(await fetchProductsList('computador'));
 
+const addLoading = () => {
+  tagH2.innerHTML = 'carregando...';
+  tagH2.classList.add('loading');
+  products.appendChild(tagH2);
+};
+
+const removeLoading = () => {
+  products.removeChild(tagH2);
+};
+
 async function criaListagemDeProdutos(produto) {
+  addLoading();
   const dataResults = await fetchProductsList(produto);
+  if (!dataResults === false) {
+    removeLoading();
+  }
 
   dataResults.forEach((dataResult) => {
     const retornoImg = createProductElement(dataResult);
     products.appendChild(retornoImg);
-    console.log(retornoImg);
   });
 }
 
